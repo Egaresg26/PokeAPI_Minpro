776a9ac2a5cae0ae958e3dcff7e0cf95c68635f1
Egaresg26 <102356277+Egaresg26@users.noreply.github.com>
2024-03-13 21:57:36 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1@172.17.0.2/pokemon_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://yosua:yosua123@35.238.161.118/pokemon_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'your_secret_key'
 
